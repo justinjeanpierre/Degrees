3c2d3e6ceea021211fa5f1f7fc5bfadc4a823ab3
justinjeanpierre <justin.jeanpierre@gmail.com>
2015-05-21 21:15:18 -0400

    Added Xcode project.

---
+++ Degrees/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Degrees
+//
+//  Created by Justin Jean-Pierre on 2015-05-21.
+//  Copyright (c) 2015 Monsoon Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
