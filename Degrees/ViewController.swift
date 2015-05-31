5f874f33104dd7ba0edfc67c3a24f2b7fa431bb3
justinjeanpierre <justin.jeanpierre@gmail.com>
2015-05-31 17:25:28 -0400

    Implemented degree conversion
    and connected buttons to view controller.

--- Degrees/ViewController.swift
+++ Degrees/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet var inputTextField: UITextField!
+	@IBOutlet var outputLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+	@IBAction func didPressToFahrenheitButton(sender: UIButton) {
+		if let degreesToConvert = inputTextField.text.toInt() {
+			let convertedDegrees = celsiusToFahrenheit(Double(degreesToConvert))
+			outputLabel.text = ("\(degreesToConvert) ˚C is \(convertedDegrees) ˚F")
+		} else {
+			outputLabel.text = "Please enter a valid temperature"
+		}
+	}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+	@IBAction func didPressToCelsiusButton(sender: UIButton) {
+		if let degreesToConvert = inputTextField.text.toInt() {
+			let convertedDegrees = fahrenheitToCelsius(Double(degreesToConvert))
+			outputLabel.text = ("\(degreesToConvert) ˚F is \(convertedDegrees) ˚C")
+		} else {
+			outputLabel.text = "Please enter a valid temperature"
+		}
+	}
+	
+	// C to F formula:
+	// Tf = Tc * (9/5) + 32
+	func celsiusToFahrenheit(degreesC: Double) -> Double {
+		return (((degreesC * 9.0) / 5.0) + 32.0)
+	}
+	
+	// F to C formula:
+	// Tc = (Tf - 32) * (5/9)
+	func fahrenheitToCelsius(degreesF: Double) -> Double {
+		return (((degreesF - 32.0) * 50.0) / 9.0)
+	}
 }
-
